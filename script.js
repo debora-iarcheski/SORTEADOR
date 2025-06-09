99b569d7d03ba9dc1fcbff4add5e47248d0cf115
debora-iarcheski <iarcheski2@gmail.com>
2025-06-09 11:54:39 -0300

    Sorteador - projeto DevClub com HTML, CSS e JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+function generateNumber(){
+
+    const min = Math.ceil(document.querySelector('.input-min').value);
+    const max = Math.floor(document.querySelector('.input-max').value);
+    
+    if(min >= max){
+        alert('O valor mínimo tem que ser MENOR que o valor máximo')
+    } else {
+        const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(resultado)
+    }
+
+}
